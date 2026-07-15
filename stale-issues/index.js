4ff3561fbb3b4465af238e7197a3b8725d16ff05
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-07-15 16:43:18 +0100

    fix: cjs module loader failure - switch to esm

--- stale-issues/index.js
+++ stale-issues/index.js
@@ -1,7 +1,5 @@
-'use strict'
-
-const core = require('@actions/core')
-const github = require('@actions/github')
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 /**
  * @typedef {Object} Threshold
